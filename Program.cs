be9b075f298144dd1a6b24896d4fa545e1b3216d
KeresztesHunor <kereszteshunor36@gmail.com>
2025-11-13 22:39:55 +0100

    SimpleLocalDB.dll

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Beadando_VM_KH
+﻿using Microsoft.Extensions.Configuration;
+using SimpleLocalDB;
+
+namespace Beadando_VM_KH
 {
     internal class Program
     {
+        static readonly IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(AppDbContext.BasePath)
+            .AddJsonFile("appconfig.json", false, true)
+            .Build()
+        ;
+
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
