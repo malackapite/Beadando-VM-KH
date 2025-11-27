8aea4ff0e2649ba78e314cdc29e527121baf0a4a
KeresztesHunor <kereszteshunor36@gmail.com>
2025-11-27 17:47:27 +0100

    SimpleLocalDB project included in project

---
+++ SimpleLocalDB/MissingConfigFileFieldException.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleLocalDB
+{
+    internal class MissingConfigFileFieldException(string fieldName) : InvalidOperationException($"Field '{fieldName}' is missing from the config file.")
+    {
+
+    }
+}
